076a741ccaab4a046e31582ebee70a27eaa29235
daun-up <awhekdns@kookmin.ac.kr>
2025-02-11 16:40:26 +0900

    chore: eslint, prettier 세팅

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+export default function Login() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-16 p-8 pb-20">
+      <div className="text-center">
+        <h1>국민대학교 소프트웨어융합대학</h1>
+        <p className="text-3xl font-bold">복지물품 대여 시스템</p>
+      </div>
+    </div>
+  );
+}
