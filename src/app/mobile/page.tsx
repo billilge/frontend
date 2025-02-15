bdcd92c8d405e46efcc4b753e232581b085ef78f
Sinji <sinzi031012@naver.com>
2025-02-16 02:48:08 +0900

    chore: hamburger 오타 수정

--- src/app/mobile/page.tsx
+++ src/app/mobile/page.tsx
@@ -8,7 +8,7 @@ export default function Mobile() {
         <h1>국민대학교 소프트웨어융합대학</h1>
         <p className="text-3xl font-bold">복지물품 대여 시스템</p>
       </div> */}
-      <Header title="관리자 대시보드" />
+      <Header title="관리자 대시보드" menu />
     </MobileLayout>
   );
 }
