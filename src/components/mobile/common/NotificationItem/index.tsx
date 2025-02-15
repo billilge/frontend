2ef734e383891e10067ba7543d280caf28816871
Sinji <sinzi031012@naver.com>
2025-02-16 04:18:02 +0900

    feat: NotificationItem 배경색, 폰트 색, 아이콘 제외 퍼블리싱

--- src/components/mobile/common/NotificationItem/index.tsx
+++ src/components/mobile/common/NotificationItem/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
 // import IconBell from 'public/assets/icon-bell.svg';
 
 interface NotificationItemProps {
@@ -15,5 +17,26 @@ export default function NotificationItem({
   status,
   date,
 }: NotificationItemProps) {
-  return <section>테스트</section>;
+  return (
+    <Link className="w-full" href={link}>
+      <section
+        className={cn('flex w-full gap-2.5 p-5', isRead ? '' : 'bg-sky-100')}
+      >
+        {/* svg 안 먹혀서 아이콘 임시 대처 */}
+        <div className="flex h-4 w-4 bg-black" />
+
+        <section className="flex w-full flex-col items-start gap-2.5 text-sm font-medium">
+          <section className="flex w-full justify-between">
+            <div className="text-[12px] font-medium">대여 신청</div>
+            <div className="text-[12px] font-medium text-gray-secondary">
+              {date}분 전
+            </div>
+          </section>
+          {message}
+        </section>
+      </section>
+    </Link>
+  );
 }
+
+// TODO : 아이콘 세팅, 글자별 색상 세팅 배경색 임시로 설정했는데 tailwind에 수정헤야 함
