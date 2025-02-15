1d9a5cc90163131deaa2bd84d59edda740ab7060
Sinji <sinzi031012@naver.com>
2025-02-15 18:40:11 +0900

    feat: svg 포함 구현

--- src/components/mobile/Header/index.tsx
+++ src/components/mobile/Header/index.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import IconArrow from 'public/assets/icon-arrow.svg';
+import IconHambuger from 'public/assets/icon-hamburger.svg';
 
 interface HeaderProps {
   title: string;
@@ -12,11 +14,25 @@ export default function Header({ title, menu = false }: HeaderProps) {
 
   return (
     <section className="flex h-8 w-full items-center justify-between px-4 py-1.5">
-      <button type="button" onClick={() => router.back()}>
-        버튼
+      <button
+        className="h-6 w-6 items-center justify-center"
+        type="button"
+        onClick={() => router.back()}
+      >
+        <IconArrow />
       </button>
-      <div className="font-normal text-black-primary">{title}</div>
-      {menu ? <button>테스트</button> : <div className="h-6 w-6" />}
+      <div className="font-medium text-black-primary">{title}</div>
+      {menu ? (
+        <button
+          className="h-6 w-6 items-center justify-center"
+          type="button"
+          onClick={() => console.log('사이드바 오픈!')}
+        >
+          <IconHambuger />
+        </button>
+      ) : (
+        <div className="h-6 w-6" />
+      )}
     </section>
   );
 }
