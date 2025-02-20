060f9d81f9a36cdf9c4bc2f030a66c86fdfe09bc
hyeonjin6530 <cola1876@naver.com>
2025-02-21 02:16:24 +0900

    feat: 대여 기록 페이지 구현

--- src/app/mobile/history/page.tsx
+++ src/app/mobile/history/page.tsx
@@ -6,6 +6,8 @@ import Header from '@/components/mobile/Header';
 // import useDropdown from '@/hooks/useDropdown';
 import ReturnItem from '@/app/mobile/history/_components/ReturnItem';
 import RentalItem from '@/app/mobile/history/_components/RentalItem';
+import Alert from '@/components/mobile/Alert';
+import IconNoReturn from '../../../../public/assets/icon-no-return.svg';
 
 export default function Mobile() {
   // const dropdownActions = [
@@ -17,114 +19,188 @@ export default function Mobile() {
   // const { showDropdown, hideDropdown, isDropdownVisible } = useDropdown();
 
   const returnItemDummy = [
-    // { name: '현진이의 감자', url: '/assets/test.svg', dayCount: 3 },
-    // { name: '감자', url: '/assets/test.svg', dayCount: 65 },
-    // { name: '고구마', url: '/assets/test.svg', dayCount: 666 },
-    // { name: '옥수수', url: '/assets/test.svg', dayCount: 656565 },
-    // { name: '콩', url: '/assets/test.svg', dayCount: 6566656 },
-    // { name: '팥', url: '/assets/test.svg', dayCount: 6 },
+    { name: '현진이의 감자', url: '/assets/icon-test.svg', dayCount: 3 },
+    { name: '감자', url: '/assets/icon-test.svg', dayCount: 65 },
+    { name: '고구마', url: '/assets/icon-test.svg', dayCount: 666 },
+    { name: '옥수수', url: '/assets/icon-test.svg', dayCount: 656565 },
+    { name: '콩', url: '/assets/icon-test.svg', dayCount: 6566656 },
+    { name: '팥', url: '/assets/icon-test.svg', dayCount: 6 },
   ];
 
   const rentalItemDummy = [
-    // {
-    //   rentalHistoryId: 1,
-    //   member: {
-    //     name: '황수민',
-    //     studentId: '20213102',
-    //   },
-    //   item: {
-    //     itemName: '고데기',
-    //     imageUrl: '/assets/test.svg',
-    //   },
-    //   rentAt: '2025.01.22 13:08',
-    //   returnedAt: '2025.01.22 13:08',
-    //   rentalStatus: 'PENDING',
-    // },
-    // {
-    //   rentalHistoryId: 2,
-    //   member: {
-    //     name: '황수민',
-    //     studentId: '20213102',
-    //   },
-    //   item: {
-    //     itemName: '고데기',
-    //     imageUrl: '/assets/test.svg',
-    //   },
-    //   rentAt: '2025.01.22 13:08',
-    //   returnedAt: '2025.01.22 13:08',
-    //   rentalStatus: 'PENDING',
-    // },
-    // {
-    //   rentalHistoryId: 3,
-    //   member: {
-    //     name: '황수민',
-    //     studentId: '20213102',
-    //   },
-    //   item: {
-    //     itemName: '짱뜨거운고데기인데요사실은그게',
-    //     imageUrl: '/assets/test.svg',
-    //   },
-    //   rentAt: '2025.01.22 13:08',
-    //   returnedAt: '',
-    //   rentalStatus: 'RENTAL',
-    // },
+    {
+      rentalHistoryId: 1,
+      member: {
+        name: '황수민',
+        studentId: '20213102',
+      },
+      item: {
+        itemName: '고데기',
+        imageUrl: '/assets/icon-test.svg',
+      },
+      rentAt: '2025.01.22 13:08',
+      returnedAt: '2025.01.22 13:08',
+      rentalStatus: 'PENDING',
+    },
+    {
+      rentalHistoryId: 2,
+      member: {
+        name: '황수민',
+        studentId: '20213102',
+      },
+      item: {
+        itemName: '고데기',
+        imageUrl: '/assets/icon-test.svg',
+      },
+      rentAt: '2025.01.22 13:08',
+      returnedAt: '2025.01.22 13:08',
+      rentalStatus: 'CANCEL',
+    },
+    {
+      rentalHistoryId: 3,
+      member: {
+        name: '황수민',
+        studentId: '20213102',
+      },
+      item: {
+        itemName: '짱뜨거운고데기인데요사실은그게',
+        imageUrl: '/assets/icon-test.svg',
+      },
+      rentAt: '2025.01.22 13:08',
+      returnedAt: '',
+      rentalStatus: 'CONFIRMED',
+    },
+    {
+      rentalHistoryId: 4,
+      member: {
+        name: '황수민',
+        studentId: '20213102',
+      },
+      item: {
+        itemName: '짱뜨거운고데기인데요사실은그게',
+        imageUrl: '/assets/icon-test.svg',
+      },
+      rentAt: '2025.01.22 13:08',
+      returnedAt: '',
+      rentalStatus: 'RENTAL',
+    },
+    {
+      rentalHistoryId: 5,
+      member: {
+        name: '황수민',
+        studentId: '20213102',
+      },
+      item: {
+        itemName: '짱뜨거운고데기인데요사실은그게',
+        imageUrl: '/assets/icon-test.svg',
+      },
+      rentAt: '2025.01.22 13:08',
+      returnedAt: '',
+      rentalStatus: 'RETURN_PENDING',
+    },
+    {
+      rentalHistoryId: 6,
+      member: {
+        name: '황수민',
+        studentId: '20213102',
+      },
+      item: {
+        itemName: '짱뜨거운고데기인데요사실은그게',
+        imageUrl: '/assets/icon-test.svg',
+      },
+      rentAt: '2025.01.22 13:08',
+      returnedAt: '',
+      rentalStatus: 'RETURN_CONFIRMED',
+    },
+    {
+      rentalHistoryId: 7,
+      member: {
+        name: '황수민',
+        studentId: '20213102',
+      },
+      item: {
+        itemName: '짱뜨거운고데기인데요사실은그게',
+        imageUrl: '/assets/icon-test.svg',
+      },
+      rentAt: '2025.01.22 13:08',
+      returnedAt: '',
+      rentalStatus: 'RETURNED',
+    },
   ];
 
   return (
     <MobileLayout>
       <Header title="대여 기록" menu />
-      <div className="pb-1.5 pl-4 pt-[22px] text-heading-4_M font-semibold">
-        반납이 필요한 물품
-      </div>
-      <div className="box-border flex gap-1.5 overflow-auto px-4 py-1">
-        {returnItemDummy.length > 0 ? (
-          returnItemDummy.map((item) => (
-            <ReturnItem
-              key={item.name}
-              name={item.name}
-              url={item.url}
-              dayCount={item.dayCount}
-            />
-          ))
-        ) : (
-          <div className="text-body-2-normal_semi font-medium">
-            반납이 필요한 물품이 없습니다.
-          </div>
-        )}
-      </div>
 
-      <div className="px-4">
-        <div className="pb-1.5 pt-[50px] text-heading-4_M font-semibold">
-          대여 내역
+      <div className="relative h-full">
+        <div className="pb-1.5 pl-4 pt-[22px] text-heading-4_M font-semibold">
+          반납이 필요한 물품
         </div>
-        {rentalItemDummy.length > 0 ? (
-          rentalItemDummy.map((item) => (
-            <RentalItem
-              key={item.rentalHistoryId}
-              item={item.item}
-              rentAt={item.rentAt}
-              returnAt={item.returnedAt}
-              rentalStatus={item.rentalStatus}
-            />
-          ))
-        ) : (
-          <div className="text-body-2-normal_semi font-medium">
-            대여 내역이 없습니다.
-          </div>
-        )}
+        <div className="box-border flex gap-1.5 overflow-auto px-4 py-1">
+          {returnItemDummy.length > 0 ? (
+            returnItemDummy.map((item) => (
+              <ReturnItem
+                key={item.name}
+                name={item.name}
+                url={item.url}
+                dayCount={item.dayCount}
+              />
+            ))
+          ) : (
+            <div className="flex h-[107px] w-full flex-col items-center justify-center gap-3">
+              <IconNoReturn />
+              <div className="text-body-2-normal_semi font-medium text-gray-secondary">
+                반납이 필요한 물품이 없습니다.
+              </div>
+            </div>
+          )}
+        </div>
+
+        <section className="px-4 pb-12 pt-[50px]">
+          <div className="pb-1.5 text-heading-4_M font-semibold">대여 내역</div>
+          {rentalItemDummy.length > 0 ? (
+            rentalItemDummy.map((item) => (
+              <RentalItem
+                key={item.rentalHistoryId}
+                item={item.item}
+                rentAt={item.rentAt}
+                returnAt={item.returnedAt}
+                rentalStatus={item.rentalStatus}
+              />
+            ))
+          ) : (
+            <div className="flex h-full flex-col items-center justify-center">
+              <div className="text-body-2-normal_semi font-medium text-gray-secondary">
+                대여 내역이 없습니다.
+              </div>
+            </div>
+          )}
+        </section>
+        {/* <Alert */}
+        {/*  content="해당 물품을 반납 하시겠습니까?" */}
+        {/*  ctaButtonText="반납하기" */}
+        {/*  otherButtonText="취소하기" */}
+        {/*  isMainColor // CTA 버튼을 메인 컬러로 */}
+        {/*  onClickCta={() => console.log('반납하기 버튼 클릭')} */}
+        {/*  onClickOther={() => console.log('취소하기 버튼 클릭')} */}
+        {/* /> */}
+
+        {/* <Dropdown */}
+        {/*  actions={dropdownActions} */}
+        {/*  isVisible={isDropdownVisible} */}
+        {/*  hideDropdown={hideDropdown} */}
+        {/*  positionClasses="top-5 right-5" */}
+        {/* /> */}
+        {/* <button type="button" onClick={showDropdown}> */}
+        {/*  열기 */}
+        {/* </button> */}
+        {/* <button type="button" onClick={hideDropdown}> */}
+        {/*  닫기 */}
+        {/* </button> */}
+        <div className="fixed bottom-0 left-0 right-0 flex h-12 items-center justify-center bg-[#F3F4F6]">
+          <div className="text-body-2-normal_medi">© wink</div>
+        </div>
       </div>
-      {/* <Dropdown */}
-      {/*  actions={dropdownActions} */}
-      {/*  isVisible={isDropdownVisible} */}
-      {/*  hideDropdown={hideDropdown} */}
-      {/*  positionClasses="top-5 right-5" */}
-      {/* /> */}
-      {/* <button type="button" onClick={showDropdown}> */}
-      {/*  열기 */}
-      {/* </button> */}
-      {/* <button type="button" onClick={hideDropdown}> */}
-      {/*  닫기 */}
-      {/* </button> */}
     </MobileLayout>
   );
 }
