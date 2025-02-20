8aa44c2f104804f88239b0d134413b48f3c25b82
hyeonjin6530 <cola1876@naver.com>
2025-02-20 21:37:55 +0900

    feat: 모바일에서만 배경색이 있도록 설정

--- src/components/mobile/layout/index.tsx
+++ src/components/mobile/layout/index.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 interface MobileLayoutProps {
   children: React.ReactNode;
 }
 
 export default function MobileLayout({ children }: MobileLayoutProps) {
-  return (
-    <div className="flex h-svh max-w-3xl flex-col bg-[#F3F4F6]">{children}</div>
-  );
+  useEffect(() => {
+    // 기존 body 배경색 저장
+    const originalBgColor = document.body.style.backgroundColor;
+
+    // MobileLayout이 마운트될 때 배경색 적용(모바일에서만 적용하기 위함)
+    document.body.style.backgroundColor = '#F3F4F6';
+
+    return () => {
+      // MobileLayout이 언마운트될 때 원래 배경색 복구
+      document.body.style.backgroundColor = originalBgColor;
+    };
+  }, []);
+
+  return <div className="flex min-h-screen max-w-3xl flex-col">{children}</div>;
 }
