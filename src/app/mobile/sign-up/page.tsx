5e9065634a586f50c90112d4d04a31a7030c4d1a
Sinji <sinzi031012@naver.com>
2025-02-21 19:43:17 +0900

    comment: API 연결 시 연결해야 할 것들 TODO 추가

--- src/app/mobile/sign-up/page.tsx
+++ src/app/mobile/sign-up/page.tsx
@@ -17,6 +17,7 @@ export default function SignUp() {
         <section className="mt-[50px] flex w-11/12 flex-col items-start gap-6">
           <section className="flex w-full flex-col gap-1.5">
             <div>이름을 입력해 주세요.</div>
+            {/* TODO : 이름 유효성 검사 */}
             <input
               placeholder="이름을 정확히 입력해 주세요."
               className="flex w-full rounded-xl border px-3.5 py-4"
@@ -24,6 +25,7 @@ export default function SignUp() {
           </section>
           <section className="flex w-full flex-col gap-1.5">
             <div>학번을 입력해 주세요.</div>
+            {/* TODO : 학번 8자 + 유효성 검사사 */}
             <input
               placeholder="8자 모두 입력해 주세요."
               className="flex w-full rounded-xl border px-3.5 py-4"
@@ -31,6 +33,7 @@ export default function SignUp() {
           </section>
         </section>
 
+        {/* TODO : onClick 연결하기 */}
         <button
           className="bg-on-kookmin absolute bottom-7 flex w-11/12 justify-between rounded-2xl border px-6 py-4 text-white-primary"
           type="button"
