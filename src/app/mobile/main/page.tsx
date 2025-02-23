bbe76a8675bf64af23433e24db8bf64f6ef97699
hyeonjin6530 <cola1876@naver.com>
2025-02-23 19:23:09 +0900

    feat: 복지물품 대여 페이지 제작

---
+++ src/app/mobile/main/page.tsx
@@ -0,0 +1,96 @@
+'use client';
+
+import MobileLayout from '@/components/mobile/layout';
+import MainHeader from '@/app/mobile/main/_components/MainHeader';
+import Carousel from '@/app/mobile/main/_components/Carousel';
+import WelfareItem from '@/app/mobile/main/_components/WelfareItem';
+
+export default function MobileMain() {
+  const imageUrls = [
+    '/assets/images/test.png',
+    '/assets/images/test2.png',
+    '/assets/images/test.png',
+  ];
+
+  const items = [
+    {
+      itemId: 12,
+      itemName: '우산',
+      itemType: 'RENTAL',
+      count: 10,
+      imageUrl: '/assets/icons/icon-test.svg',
+    },
+    {
+      itemId: 11,
+      itemName: '감자',
+      itemType: 'RENTAL',
+      count: 10,
+      imageUrl: '/assets/icons/icon-test.svg',
+    },
+    {
+      itemId: 13,
+      itemName: '왕연진',
+      itemType: 'RENTAL',
+      count: 10,
+      imageUrl: '/assets/icons/icon-test.svg',
+    },
+    {
+      itemId: 3,
+      itemName: '왕연진',
+      itemType: 'RENTAL',
+      count: 10,
+      imageUrl: '/assets/icons/icon-test.svg',
+    },
+    {
+      itemId: 1,
+      itemName: '왕연진',
+      itemType: 'RENTAL',
+      count: 10,
+      imageUrl: '/assets/icons/icon-test.svg',
+    },
+    {
+      itemId: 14,
+      itemName: '현진',
+      itemType: 'RENTAL',
+      count: 10,
+      imageUrl: '/assets/icons/icon-test.svg',
+    },
+    {
+      itemId: 15,
+      itemName: '황현진',
+      itemType: 'RENTAL',
+      count: 10,
+      imageUrl: '/assets/icons/icon-test.svg',
+    },
+    {
+      itemId: 16,
+      itemName: '진',
+      itemType: 'RENTAL',
+      count: 10,
+      imageUrl: '/assets/icons/icon-test.svg',
+    },
+  ];
+
+  return (
+    <MobileLayout>
+      <MainHeader name="황현진" />
+      <div className="mt-10 flex flex-col gap-[50px] px-4 pt-4">
+        <Carousel images={imageUrls} />
+
+        <section className="flex flex-col gap-4">
+          <div className="text-heading-4_M font-semibold">복지 물품 목록</div>
+          <div className="flex flex-col gap-[9px]">
+            {items.map((item) => (
+              <WelfareItem
+                key={item.itemId}
+                itemName={item.itemName}
+                imageUrl={item.imageUrl}
+                count={item.count}
+              />
+            ))}
+          </div>
+        </section>
+      </div>
+    </MobileLayout>
+  );
+}
