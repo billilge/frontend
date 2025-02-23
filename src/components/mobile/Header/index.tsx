ace56fb3021519bc4ad88df2d0c166f330ec88b0
hyeonjin6530 <cola1876@naver.com>
2025-02-23 18:24:27 +0900

    feat: 헤더에 sidebar 연결

--- src/components/mobile/Header/index.tsx
+++ src/components/mobile/Header/index.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import Sidebar from '@/components/mobile/SidebarMenu/index';
+import { useState } from 'react';
 import IconArrow from 'public/assets/icons/icon-arrow.svg';
 import IconHamburger from 'public/assets/icons/icon-hamburger.svg';
 
@@ -11,28 +13,33 @@ interface HeaderProps {
 
 export default function Header({ title, menu = false }: HeaderProps) {
   const router = useRouter();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
-    <section className="h-8.5 flex w-full items-center justify-between px-4 py-1.5">
-      <button
-        className="h-6 w-6 items-center justify-center"
-        type="button"
-        onClick={() => router.back()}
-      >
-        <IconArrow />
-      </button>
-      <div className="font-medium leading-6 text-black-primary">{title}</div>
-      {menu ? (
+    <>
+      <section className="h-8.5 flex w-full items-center justify-between px-4 py-1.5">
         <button
           className="h-6 w-6 items-center justify-center"
           type="button"
-          onClick={() => console.log('사이드바 오픈!')}
+          onClick={() => router.back()}
         >
-          <IconHamburger />
+          <IconArrow />
         </button>
-      ) : (
-        <div className="h-6 w-6" />
-      )}
-    </section>
+        <div className="font-medium leading-6 text-black-primary">{title}</div>
+        {menu ? (
+          <button
+            className="h-6 w-6 items-center justify-center"
+            type="button"
+            onClick={() => setIsSidebarOpen(true)}
+          >
+            <IconHamburger />
+          </button>
+        ) : (
+          <div className="h-6 w-6" />
+        )}
+      </section>
+
+      <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
+    </>
   );
 }
