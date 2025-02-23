f704e63830e0f42717a8e04155715868885c6609
Sinji <sinzi031012@naver.com>
2025-02-24 02:10:54 +0900

    feat: 로그인 이후 헤더 추가하도록 수정

--- src/services/publicAxiosInstance.ts
+++ src/services/publicAxiosInstance.ts
@@ -1,17 +1,42 @@
 import axios, { InternalAxiosRequestConfig } from 'axios';
 
+const getAccessToken = (): string | null => {
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    try {
+      return token || '';
+    } catch (error) {
+      console.error('Failed to parse user from localStorage', error);
+      return '';
+    }
+  }
+  return '';
+};
+
 const PublicAxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URI,
-  headers: { 'Content-Type': 'application/json' },
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${getAccessToken()}`,
+  },
   withCredentials: true,
 });
 
 PublicAxiosInstance.interceptors.request.use(
   (config: InternalAxiosRequestConfig) => {
-    return {
+    const token = getAccessToken();
+
+    const newConfig = {
       ...config,
       withCredentials: false,
     };
+
+    if (token) {
+      newConfig.headers.set('Authorization', `Bearer ${token}`);
+    }
+
+    return newConfig;
   },
   (error) => Promise.reject(error),
 );
