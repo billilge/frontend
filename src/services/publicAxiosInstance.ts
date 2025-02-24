279039138d8203e72ec75f884cf23d168ce24a0f
daun-up <awhekdns@kookmin.ac.kr>
2025-02-24 18:09:47 +0900

    feat: 커스텀 AxiosInstance 추가

---
+++ src/services/publicAxiosInstance.ts
@@ -0,0 +1,19 @@
+import axios, { InternalAxiosRequestConfig } from 'axios';
+
+const PublicAxiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URI,
+  // timeout: 2000,
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
+});
+
+PublicAxiosInstance.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    return {
+      ...config,
+      withCredentials: false,
+    };
+  },
+  (error) => Promise.reject(error),
+);
+export default PublicAxiosInstance;
