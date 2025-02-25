8d4979f76b414915a64d3cfbc945b6557b224f32
Sinji <sinzi031012@naver.com>
2025-02-26 03:15:04 +0900

    feat: 사용자 알림 읽음 API 연결

--- src/app/mobile/notification/page.tsx
+++ src/app/mobile/notification/page.tsx
@@ -2,7 +2,10 @@
 
 import { useState, useEffect } from 'react';
 import MobileLayout from '@/components/mobile/layout';
-import { userNotificationGet } from '@/services/notification';
+import {
+  userNotificationGet,
+  readNotificationPost,
+} from '@/services/notification';
 import NotificationItem from '@/components/mobile/NotificationItem';
 import Header from '@/components/mobile/Header';
 import { elapsedTime } from '@/utils/elapsedTime';
@@ -26,6 +29,10 @@ export default function Notification() {
 
   // TODO : 로그인 여부 확인 후 로그인 안 했으면 로그인 화면으로 보내기
 
+  const handleReadNotification = async (notificationId: number) => {
+    await readNotificationPost(notificationId);
+  };
+
   return (
     <MobileLayout>
       <Header title="알림" />
@@ -42,6 +49,9 @@ export default function Notification() {
             isRead={item.isRead}
             status={item.status}
             createdAt={elapsedTime(item.createdAt)}
+            handleNotification={() =>
+              item.notificationId && handleReadNotification(item.notificationId)
+            }
           />
         ))
       )}
