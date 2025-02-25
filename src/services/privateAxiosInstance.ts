399dfa410e720c211c77c8de3543c2dd4102fc9a
Sinji <sinzi031012@naver.com>
2025-02-25 21:11:56 +0900

    feat: API 헤더 관련 설정 수정

--- src/services/privateAxiosInstance.ts
+++ src/services/privateAxiosInstance.ts
@@ -1,10 +1,24 @@
 import axios, { InternalAxiosRequestConfig } from 'axios';
 
+const getAccessToken = (): string | null => {
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    try {
+      return token || '';
+    } catch (error) {
+      console.error('Failed to parse user from localStorage', error);
+      return '';
+    }
+  }
+  return '';
+};
+
 const PrivateAxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URI,
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_AUTH_TOKEN}`,
+    Authorization: `Bearer ${getAccessToken()}`,
   },
 });
 
--- src/services/publicAxiosInstance.ts
+++ src/services/publicAxiosInstance.ts
@@ -1,42 +1,16 @@
 import axios, { InternalAxiosRequestConfig } from 'axios';
 
-const getAccessToken = (): string | null => {
-  const token = localStorage.getItem('token');
-
-  if (token) {
-    try {
-      return token || '';
-    } catch (error) {
-      console.error('Failed to parse user from localStorage', error);
-      return '';
-    }
-  }
-  return '';
-};
-
 const PublicAxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URI,
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${getAccessToken()}`,
-  },
+  headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 });
-
 PublicAxiosInstance.interceptors.request.use(
   (config: InternalAxiosRequestConfig) => {
-    const token = getAccessToken();
-
-    const newConfig = {
+    return {
       ...config,
       withCredentials: false,
     };
-
-    if (token) {
-      newConfig.headers.set('Authorization', `Bearer ${token}`);
-    }
-
-    return newConfig;
   },
   (error) => Promise.reject(error),
 );
