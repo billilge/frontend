e4bf88be3e412e3984d10f3cb54822a1954afaf9
Sinji <sinzi031012@naver.com>
2025-03-02 22:11:22 +0900

    design: 헤더 아이콘 크기 1.25배

--- src/components/mobile/Header/index.tsx
+++ src/components/mobile/Header/index.tsx
@@ -31,25 +31,25 @@ export default function Header({ title, menu = false }: HeaderProps) {
     <>
       <section className="sticky top-0 flex h-9 w-full items-center justify-between bg-[#F3F4F6] px-4 py-1.5">
         <button
-          className="h-7 w-7 items-center justify-center"
+          className="h-6 w-6 items-center justify-center"
           type="button"
           onClick={() => router.back()}
         >
-          <IconArrow />
+          <IconArrow className="scale-125" />
         </button>
         <div className="font-semibold leading-6 text-black-primary">
           {title}
         </div>
         {menu ? (
           <button
-            className="h-7 w-7 items-center justify-center"
+            className="h-6 w-6 items-center justify-center"
             type="button"
             onClick={() => setIsSidebarOpen(true)}
           >
-            <IconHamburger />
+            <IconHamburger className="scale-125" />
           </button>
         ) : (
-          <div className="h-7 w-7" />
+          <div className="h-6 w-6" />
         )}
       </section>
 
