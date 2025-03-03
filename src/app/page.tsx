71f3fda44b55ebddffcba5792efdfa1c8679db00
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-03-04 00:43:31 +0900

    design: 루트 페이지 꾸미기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import ImageLoginLogo from '../../public/assets/images/image-login-logo.svg';
+
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/mobile/sign-in');
+  }, []);
+
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      빌릴게예
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <section className="mt-[128px] flex flex-col items-center justify-start gap-1.5">
+        <div className="text-2xl font-normal">
+          국민대학교 소프트웨어융합대학
+        </div>
+        <div className="text-3xl font-semibold">복지물품 대여 시스템</div>
+      </section>
+      <ImageLoginLogo className="absolute top-24 rounded-2xl" />
     </div>
   );
 }
