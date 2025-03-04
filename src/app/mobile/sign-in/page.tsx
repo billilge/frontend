d03a6c24ddcc1338ddb369c00aa633df04304e10
Sinji <sinzi031012@naver.com>
2025-03-04 16:24:34 +0900

    feat: 에브리타임, 인스타그램 인앱 브라우저 분기 처리

--- src/app/mobile/sign-in/page.tsx
+++ src/app/mobile/sign-in/page.tsx
@@ -2,14 +2,73 @@
 
 import ImageLoginLogo from 'public/assets/images/image-login-logo.svg';
 import IconGoogle from 'public/assets/icons/icon-google.svg';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { getUA } from 'react-device-detect';
+import Alert from '@/components/mobile/Alert';
 
 export default function SignIn() {
   const router = useRouter();
+  const [alertState, setAlertState] = useState(false);
+
+  const handleAlertOpen = () => {
+    setAlertState(true);
+  };
+
+  const handleAlertClose = () => {
+    setAlertState(false);
+  };
+
+  const handleInAppBrowser = () => {
+    const userAgent = navigator.userAgent.toLowerCase();
+    const redirectToExternalBrowser = () => {
+      const targetUrl = 'https://billilge.site/mobile/sign-in';
+
+      if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+        const safariUrl = `safari://${targetUrl.replace(/https?:\/\//i, '')}`;
+        window.location.href = safariUrl;
+      } else {
+        window.location.href = `intent://${targetUrl.replace(
+          /https?:\/\//i,
+          '',
+        )}#Intent;scheme=http;package=com.android.chrome;end`;
+      }
+    };
+
+    if (/kakaotalk/i.test(userAgent)) {
+      window.location.href = `kakaotalk://web/openExternal?url=${encodeURIComponent(
+        'https://billilge.site/mobile/sign-in',
+      )}`;
+    } else if (/line/i.test(userAgent)) {
+      const targetUrl = 'https://billilge.site/mobile/sign-in';
+      window.location.href = targetUrl.includes('?')
+        ? `${targetUrl}&openExternalBrowser=1`
+        : `${targetUrl}?openExternalBrowser=1`;
+    } else if (
+      /inapp|snapchat|wirtschaftswoche|thunderbird|instagram|everytimeapp|whatsApp|electron|wadiz|aliapp|zumapp|iphone.*whale|android.*whale|kakaostory|band|twitter|DaumApps|DaumDevice\/mobile|FB_IAB|FB4A|FBAN|FBIOS|FBSS|trill\/[^1]/i.test(
+        userAgent,
+      )
+    ) {
+      redirectToExternalBrowser();
+    }
+  };
 
   const handleLogin = () => {
-    window.location.href = `${process.env.NEXT_PUBLIC_API_BASE_URI}/oauth2/authorization/google`;
+    if (
+      getUA.match(
+        /inapp|KAKAOTALK|FBAV|Line|Instagram|wadiz|kakaostory|band|twitter|DaumApps|everytimeapp|whatsApp|electron|aliapp|zumapp|iphone.*whale|android.*whale|DaumDevice\/mobile|FB_IAB|FB4A|FBAN|FBIOS|FBSS|trill/i,
+      )
+    ) {
+      console.log('인앱 브라우저');
+      handleAlertOpen();
+    } else {
+      window.location.href = `${process.env.NEXT_PUBLIC_API_BASE_URI}/oauth2/authorization/google`;
+    }
+  };
+
+  const handleAlertConfirm = () => {
+    handleInAppBrowser();
+    navigator.clipboard.writeText('https://billilge.site/mobile/sign-in');
   };
 
   useEffect(() => {
@@ -36,6 +95,15 @@ export default function SignIn() {
         <div className="flex font-medium">Google로 시작하기</div>
         <div className="h-6 w-6" />
       </button>
+      {alertState && (
+        <Alert
+          content="인앱 브라우저는 구글 로그인을 사용할 수 없습니다."
+          ctaButtonText="url 복사하기"
+          otherButtonText="닫기"
+          onClickCta={handleAlertConfirm}
+          onClickOther={handleAlertClose}
+        />
+      )}
     </section>
   );
 }
